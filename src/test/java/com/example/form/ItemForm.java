8a5e49024590eb511587893e8c154d30dfb7f8dd
maadt <147260993+maadt@users.noreply.github.com>
2024-01-11 17:59:25 +0900

    コントローラーまで実装完了

---
+++ src/test/java/com/example/form/ItemForm.java
@@ -0,0 +1,25 @@
+package com.example.form;
+
+// 操作を行うデータのsetterとgetterを記述
+// IDはパス変数から取得するので不要
+
+public class ItemForm {
+	
+    private String name;
+
+    private Integer price;
+
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getPrice() {
+        return this.price;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
