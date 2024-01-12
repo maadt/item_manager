68d7a6db9b545293019d4c771050c5a222f278ce
maadt <147260993+maadt@users.noreply.github.com>
2024-01-12 10:55:24 +0900

    Serviceクラスの実装完了

---
+++ src/main/java/com/example/service/ItemService.java
@@ -0,0 +1,26 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Item;
+import com.example.repository.ItemRepository;
+
+
+@Service // ビジネスロジックを実装し、アプリケーションの機能を提供するためのクラス
+public class ItemService {
+	
+	private final ItemRepository itemRepository;
+	  // ItemRepositoryの実装クラスのインスタンスを保持する
+	
+	@Autowired //引数に紐づくクラスのインスタンスを生成し、利用する
+	public ItemService(ItemRepository itemRepository) { //コンストラクタ・インジェクション
+		this.itemRepository = itemRepository;
+	}
+	
+	public List<Item> findAll() { //データベース内のすべてのアイテムを取得して、アイテムのリストを返す
+		return this.itemRepository.findAll(); //データベースからすべてのアイテムを取得し、その結果を呼び出し元に返す
+	}
+}
