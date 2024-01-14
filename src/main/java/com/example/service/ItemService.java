04a333c7785b62d50b9b1ebfc1d589e818fddb7e
maadt <147260993+maadt@users.noreply.github.com>
2024-01-14 11:09:53 +0900

    データ更新処理の解説
    データ更新処理の解説を追加

--- src/main/java/com/example/service/ItemService.java
+++ src/main/java/com/example/service/ItemService.java
@@ -21,12 +21,14 @@ public class ItemService {
 		this.itemRepository = itemRepository;
 	}
 	
+	// データ一覧表示処理
 	public List<Item> findAll() { 
 		// List ... 順序を持つ要素の集合
 		// Item ... Entityクラスの型
 		return this.itemRepository.findAll(); //データベースからすべてのアイテムを取得し、その結果を呼び出し元に返す
 	}
 	
+	// データ挿入処理
 	public Item save(ItemForm itemForm) {
 	// save() ... DBに保存するためのメソッド
 	// ItemForm itemForm ... コントローラーで指定したモデル属性名
@@ -37,10 +39,11 @@ public class ItemService {
 		return this.itemRepository.save(item); // itemRepositoryを介してDBにデータが保存される
 	}
 	
+	// データ取得処理
 	public Item findById(Integer id) { 
 	// findById(Integer id) ... コントローラーからidを受け取る
 		Optional<Item> optionalItem = this.itemRepository.findById(id);
-		// this.itemRepository.findById(id) ... JPAのfindByIdメソッドを使用してidに対応したデータを取得
+		// this.itemRepository.findById(id) ... ItemRepositoryインターフェイスを介してidに対応したデータを取得
 		// Optional ... データが見つからなかった場合でもnullではなくOptional.empty()を返す
 		// <Item> ... 取得したデータをItemクラスのフィールドに代入
 		// optionalItem ... Itemクラスのオブジェクトを保持する
@@ -50,10 +53,15 @@ public class ItemService {
 		// 保持したデータを返す
 	}
 	
+	//データ更新処理
 	public Item update(Integer id, ItemForm itemForm) {
+		// update ... ItemRepositoryインターフェイスを介してデータの更新処理を行う
+		// Integer id ... サービスクラスから商品IDを受け取る
+		// ItemForm itemForm ... サービスクラスから商品情報を受け取る
 		Item item = this.findById(id);
-		item.setName(itemForm.getName());
+		// this.findById(id) ... クラス内のfindByIdメソッドを実行
+		item.setName(itemForm.getName()); // 追加したモデル属性の情報を取得してエンティティクラスにセット
 		item.setPrice(itemForm.getPrice());
-		return this.itemRepository.save(item);
+		return this.itemRepository.save(item); // itemRepositoryを介してDBにデータが保存される
 	}
 }
